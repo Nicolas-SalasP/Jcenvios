4d56218a793ab241408c10d77edf685f69682c08
Nicolas-SalasP <nicolas.salas.contacto@gmail.com>
2026-05-19 18:26:38 -0400

    Fix: Recuperacion de contraseña

--- public_html/reset-password.php
+++ public_html/reset-password.php
@@ -1,7 +1,8 @@
 <?php
 require_once __DIR__ . '/../remesas_private/src/core/init.php';
 $token = $_GET['token'] ?? '';
-$stmt = $conexion->prepare("SELECT * FROM PasswordResets WHERE Token = ? AND Used = FALSE AND ExpiresAt > NOW()");
+
+$stmt = $conexion->prepare("SELECT * FROM passwordresets WHERE Token = ? AND Used = FALSE AND ExpiresAt > NOW()");
 $stmt->bind_param("s", $token);
 $stmt->execute();
 $resultado = $stmt->get_result();
