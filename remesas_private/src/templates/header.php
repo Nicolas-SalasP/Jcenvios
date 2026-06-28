8e2f3779665048e0bd700cb496aec17cd7d4da64
Nicolas-SalasP <nicolas.salas.contacto@gmail.com>
2026-06-27 22:08:56 -0400

    feat: add Manuales link to admin nav dropdown

--- remesas_private/src/templates/header.php
+++ remesas_private/src/templates/header.php
@@ -258,6 +258,7 @@ if ($is_logged_in && isset($_SESSION['user_photo_url'])) {
                                         <li><a class="dropdown-item" href="<?php echo BASE_URL; ?>/admin/tasas.php"><i class="bi bi-currency-exchange text-warning me-2"></i> Tasas</a></li>
                                         <li><a class="dropdown-item" href="<?php echo BASE_URL; ?>/admin/feriados.php"><i class="bi bi-calendar-event text-danger me-2"></i> Feriados</a></li>
                                         <li><a class="dropdown-item" href="<?php echo BASE_URL; ?>/admin/logs.php"><i class="bi bi-clipboard-data text-secondary me-2"></i> Bitácora</a></li>
+                                        <li><a class="dropdown-item" href="<?php echo BASE_URL; ?>/admin/manuales.php"><i class="bi bi-book text-primary me-2"></i> Manuales</a></li>
                                     </ul>
                                 </li>
                                 <li class="nav-item"><a class="nav-link"
